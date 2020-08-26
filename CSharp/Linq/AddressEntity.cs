921fb55a1f0d5c40150d482fd733c9bbaf6ce726
Frank Pommerening <frank@pommerening-online.de>
2020-08-26 18:30:45 +0200

    IEquatable für AddressEnity

--- CSharp/Linq/AddressEntity.cs
+++ CSharp/Linq/AddressEntity.cs
@@ -1,6 +1,8 @@
-﻿namespace GW.AspNetTraining.LinqPlayground
+﻿using System;
+
+namespace GW.AspNetTraining.LinqPlayground
 {
-    public class AddressEntity
+    public class AddressEntity : IEquatable<AddressEntity>
     {
         public string Street { get; set; }
 
@@ -9,5 +11,38 @@
         public string ZipCode { get; set; }
 
         public string City { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is AddressEntity adr)
+            {
+                return Equals(adr);
+            }
+            return false;
+        }
+
+        public bool Equals(AddressEntity other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(other.City, City) &&
+                   string.Equals(other.Number, Number) &&
+                   string.Equals(other.ZipCode, ZipCode) &&
+                   string.Equals(other.Street, Street);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (Street != null ? Street.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Number != null ? Number.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (ZipCode != null ? ZipCode.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (City != null ? City.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        
     }
 }
