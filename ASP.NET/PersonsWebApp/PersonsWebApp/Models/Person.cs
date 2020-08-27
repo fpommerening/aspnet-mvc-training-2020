bb388c49cd043a15b42a38af6ef32d76db03bf16
Frank Pommerening <frank@pommerening-online.de>
2020-08-27 13:48:56 +0200

    FlexBox für Editfelder

--- ASP.NET/PersonsWebApp/PersonsWebApp/Models/Person.cs
+++ ASP.NET/PersonsWebApp/PersonsWebApp/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +10,16 @@ namespace FP.AspNetTraining.PersonsWebApp.Models
     {
         public Guid Id { get; set; }
 
+        [DisplayName("Name")]
         public string Name { get; set; }
 
+        [DisplayName("Vorname")]
         public string FirstName { get; set; }
 
+        [DisplayName("Geburtstag")]
         public DateTime Birthday { get; set; }
 
+        [DisplayName("Bundesland")]
         public string Location { get; set; }
 
         public bool IsNew { get; set; }
