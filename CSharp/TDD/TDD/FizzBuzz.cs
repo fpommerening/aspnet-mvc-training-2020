158523c9c1aef4436353d507aeab90cb46c4129e
Frank Pommerening <frank@pommerening-online.de>
2020-09-08 12:46:19 +0200

    Add TDD

---
+++ CSharp/TDD/TDD/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TDD
+{
+    internal class FizzBuzz
+    {
+        public FizzBuzz()
+        {
+        }
+
+        internal string Calc(int zahl)
+        {
+            var result = "";
+            if (zahl % 3 == 0)
+            {
+                result += "Fizz";
+            }
+            if(zahl % 5 == 0)
+            {
+                result += "Buzz";
+            }
+            return string.IsNullOrEmpty(result) ? zahl.ToString() : result;
+        }
+    }
+}
---
+++ CSharp/TDD/TDD/FizzBuzzTests.cs
@@ -0,0 +1,59 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        FizzBuzz _sut;
+
+        [SetUp]
+        public void Init()
+        {
+            _sut = new FizzBuzz();
+        }
+
+        [Test]
+        public void WennEineZahlÜbergebenWirdTextZurückgegeben()
+        {
+            var result = _sut.Calc(1);
+            result.Should().Be("1");
+        }
+
+        //[Test]
+        //public void Wenn2ÜbergebenWirdZweiZurückgeben()
+        //{
+        //    var result = _sut.Calc(2);
+        //    result.Should().Be("2");
+        //}
+
+        [Test]
+        public void WennGanzzahligDurchDreiTeilbarUndNichtGanzzahligDurchFünfFizzZurückgeben()
+        {
+            var result = _sut.Calc(3);
+            result.Should().Be("Fizz");
+        }
+
+        [Test]
+        public void WennGanzzahligDurchFünfTeilbarBuzzZurückgeben()
+        {
+            var result = _sut.Calc(5);
+            result.Should().Be("Buzz");
+        }
+
+        [Test]
+        public void WennGanzzahligDurchFünfzehnTeilbarFizzBuzzZurückgeben()
+        {
+            var result = _sut.Calc(15);
+            result.Should().Be("FizzBuzz");
+        }
+
+        
+    }
+}
