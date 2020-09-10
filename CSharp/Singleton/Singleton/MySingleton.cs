ac571970b97af78baa021f392b317485966a9497
Frank Pommerening <frank@pommerening-online.de>
2020-09-10 08:32:56 +0200

    Singleton

---
+++ CSharp/Singleton/Singleton/MySingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class MySingleton
+    {
+        private MySingleton()
+        {
+        }
+
+        private static MySingleton _instance;
+        private static object syncRoot = new object();
+
+        public static MySingleton Instance
+        {
+            get
+            {
+                lock (syncRoot)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new MySingleton();
+                    }
+                }
+                return _instance;
+
+            }
+        }
+    }
+}
